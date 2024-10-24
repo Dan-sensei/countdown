be5fbb8a0be9e537d34f04963c82928e361a0070
Danny Gabriel Rivera Solorzano <dan.senssei@gmail.com>
2024-10-24 20:15:13 +0200

    Mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <div className={`${oxanium.className} relative min-h-[100vh] flex flex-col overflow-hidden`}>
             <div className="flex justify-center relative">
-                <div className="relative min-w-[1120px] w-full pointer-events-none">
+                <div className="hidden lg:block relative min-w-[1120px] w-full pointer-events-none">
                     <img className="absolute translate-y-10 -translate-x-10 rotate-3 right-0 shadow-dark ml-auto" src="/confia.png" alt="Confia" />
                     <img
                         className="absolute translate-y-10 -translate-x-20 rotate-3 left-0 -scale-x-100 shadow-dark mr-auto"
@@ -19,14 +19,30 @@ export default function Home() {
                         className="absolute translate-y-96 -translate-x-20 rotate-3 right-0 top-96 -scale-x-100 drop-shadow mr-auto"
                         src="/kermit.png"
                         alt="Confia"
-						width={300}
+                        width={300}
                     />
                 </div>
+            </div>
+            <div className="block lg:hidden">
+                <img className="absolute translate-y-10 -translate-x-10 rotate-3 right-0 shadow-dark ml-auto" src="/confia.png" alt="Confia" />
             </div>
             <main className="flex flex-col flex-1 h-full items-center justify-center">
                 <div className="text-center text-xl tracking-widest text-gray-400">FIRMA DEL PISO</div>
                 <Countdown className="text-4xl lg:text-8xl" colorFrom="text-yellow-400" type="simple" targetList={list} />
             </main>
+            <div className="block lg:hidden">
+					<img
+                        className="absolute translate-y-72  rotate-3 right-0 top-96 -scale-x-100 drop-shadow mr-auto"
+                        src="/kermit.png"
+                        alt="Confia"
+                        width={300}
+                    />
+                <img
+                    className=" rotate-3 left-0 -scale-x-100 shadow-dark mr-auto"
+                    src="/evil_pepe.png"
+                    alt="Confia"
+                />
+            </div>
         </div>
     );
 }
